a20696bb0e003b9d4ecf6350c465b9392a05cbb2
jaroslaw <jarek.skrzypczak@o2.pl>
2018-10-09 22:25:14 +0200

    Add Task class Add Instance class both are simple implementation

---
+++ src/main/java/Instance.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Instance {
+    private ArrayList<Task> listOfTask;
+
+    public Instance(int size){
+        listOfTask = new ArrayList<Task>(size);
+    }
+}
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+    private int duration;
+    private int tooEarlyMultiplier;
+    private int tooLateMultiplier;
+    private int timeStart;
+
+    public Task(int duration, int tooEarlyMultiplier, int tooLateMultiplier) {
+        this.duration = duration;
+        this.tooEarlyMultiplier = tooEarlyMultiplier;
+        this.tooLateMultiplier = tooLateMultiplier;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getTooEarlyMultiplier() {
+        return tooEarlyMultiplier;
+    }
+
+    public int getTooLateMultiplier() {
+        return tooLateMultiplier;
+    }
+
+    public int getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(int timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public int getTimeEnd() {
+        return timeStart + duration;
+    }
+}
