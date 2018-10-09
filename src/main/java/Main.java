f7234699bdb7f2bbac722c354c5fc437e3c0f86b
jaroslaw <jarek.skrzypczak@o2.pl>
2018-10-09 22:38:45 +0200

    Add hint when params are wrong

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         if (args.length == 2) {
             System.out.println(args[0]+ " " + args[1]);
         }else{
-            System.out.println("You didn't add params.");
+            System.out.println("You didn't add params. File with instances and number of instance.");
         }
     }
 }
