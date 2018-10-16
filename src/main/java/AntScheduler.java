a270b5ed343838c3ef4cd3eda7e1188b31e0c545
jaroslaw <jarek.skrzypczak@o2.pl>
2018-10-16 11:05:03 +0200

    Add class ant scheduler add class pheromone matrix to use in ant scheduler

---
+++ src/main/java/AntScheduler.java
@@ -0,0 +1,11 @@
+public class AntScheduler {
+    private Instance originInstance;
+    private PheromoneMatrix pheromoneMatrix;
+
+    public AntScheduler(Instance originInstance) {
+        this.originInstance = originInstance;
+    }
+
+
+
+}
---
+++ src/main/java/PheromoneMatrix.java
@@ -0,0 +1,23 @@
+public class PheromoneMatrix {
+    private final static double EVAPORATE_MULTIPLIER = 0.9;
+
+    double[][] matrix; //[] row|from , [] column|to
+
+    public PheromoneMatrix(int size) {
+        this.matrix = new double[size][size];
+        for (int i = 0; i<size; ++i){
+            for (int j = 0; j<size; ++j){
+                matrix[i][j] = 0.0;
+            }
+        }
+    }
+
+    public void evaporatesPheromone(){
+        for (int i = 0; i<matrix.length; ++i){
+            for (int j = 0; j<matrix[i].length; ++j){
+                matrix[i][j] *= EVAPORATE_MULTIPLIER;
+            }
+        }
+    }
+
+}
