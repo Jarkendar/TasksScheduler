19d51b45a25915e208a152c5cef950cef40ceb84
jaroslaw <jarek.skrzypczak@o2.pl>
2018-10-23 11:09:59 +0200

    Modify main to work with 3 parameters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length == 2) {
-            System.out.println(args[0]+ " " + args[1]);
+        if (args.length == 3) {
+            System.out.println(args[0]+ " " + args[1]+ " " + args[2]);
+            double h = Double.parseDouble(args[2]);
             Instance instance = new FileManager().readInstanceFromFile(args[0],Integer.parseInt(args[1]));
-            new FileManager().writeInstanceToFile(args[0], Integer.parseInt(args[1]), new AntScheduler(instance, 10 * 60 * 1000).scheduleTask(0.2), 0);
+            new FileManager().writeInstanceToFile(args[0].split("[.]")[0], Integer.parseInt(args[1]), new AntScheduler(instance, 1 * 1000).scheduleTask(h), h);
         }else{
             System.out.println("You didn't add params. File with instances and number of instance.");
         }
     }
 }
+
+
+//ograniczenie złożoności do O(n^2)
