6b86f43ada1551a95cfdd41461fd689fb10a1f68
jaroslaw <jarek.skrzypczak@o2.pl>
2018-10-24 08:52:05 +0200

    Add method to optimize result by swap neighbour tasks

--- src/main/java/RatioScheduler.java
+++ src/main/java/RatioScheduler.java
@@ -15,21 +15,45 @@ public class RatioScheduler extends Scheduler {
     @Override
     public Instance scheduleTask(double h) {
         Task[] tasks = getOriginInstance().getTasks();
-        sortOfRatio(tasks);
-        betterEarlier.sort(Comparator.comparingDouble(Task::getRatio));
-        betterLater.sort(Comparator.comparingDouble(Task::getRatio));
+        sortOfRatio(tasks, getOriginInstance().getDurationSum());
+        betterEarlier.sort(Comparator.comparingDouble(Task::getTooEarlyMultiplier));
+        betterLater.sort(Comparator.comparingDouble(Task::getTooLateMultiplier).reversed());
         LinkedList<Task> result = new LinkedList<>(betterEarlier);
         result.addAll(result.size(), betterLater);
-        return new Instance(result.toArray(new Task[0])).expand();
+        Instance resultInstance = new Instance(result.toArray(new Task[0])).expand();
+        canSwapNeighbourTask(resultInstance, getOriginInstance().getDurationSum());
+        return resultInstance;
     }
 
-    private void sortOfRatio(Task[] tasks){
+    private void sortOfRatio(Task[] tasks, int border){
         for (Task task : tasks) {
             if (task.getRatio() < 1.0) {
-                betterEarlier.addFirst(task);
+                if (fitBeforeBorder(border, task)) {
+                    betterEarlier.addFirst(task);
+                }else {
+                    betterLater.addFirst(task);
+                }
             } else {
                 betterLater.addFirst(task);
             }
         }
+    }
+
+    private void canSwapNeighbourTask(Instance instance, int boundary){
+        for (int i = 1;  i <  instance.getSizeInstance()-1; i++){
+            Instance tmp = instance.clone();
+            tmp.swapTasks(i-1, i);
+            if (instance.calcCost(boundary) > tmp.expand().calcCost(boundary)){
+                instance = tmp;
+            }
+        }
+    }
+
+    private boolean fitBeforeBorder(int border, Task task){
+        int sum = 0;
+        for (Task t : betterEarlier){
+            sum += t.getDuration();
+        }
+        return sum + task.getDuration() <= border;
     }
 }
