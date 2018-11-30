b0859aabc7e739bcc4635cca377f8506f3b43617
jaroslaw <jarek.skrzypczak@o2.pl>
2018-11-30 23:21:59 +0100

    Change sort to better with use two params Del comments code

--- src/main/java/RatioScheduler.java
+++ src/main/java/RatioScheduler.java
@@ -1,4 +1,3 @@
-import java.util.Comparator;
 import java.util.LinkedList;
 
 public class RatioScheduler extends Scheduler {
@@ -15,51 +14,58 @@ public class RatioScheduler extends Scheduler {
     @Override
     public Instance scheduleTask(double h) {
         Task[] tasks = getOriginInstance().getTasks();
-        int boundary = (int)(getOriginInstance().getDurationSum()*h);
+        int boundary = (int) (getOriginInstance().getDurationSum() * h);
         sortOfRatio(tasks, boundary);
-        betterEarlier.sort(Comparator.comparingDouble(Task::getTooEarlyMultiplier));
-        betterLater.sort(Comparator.comparingDouble(Task::getTooLateMultiplier).reversed());
+        betterEarlier.sort((p1, p2) -> {
+            if (p1.getTooEarlyMultiplier() == p2.getTooEarlyMultiplier()) {
+                return -Integer.compare(p1.getDuration(), p2.getDuration());
+            } else {
+                return Integer.compare(p1.getTooEarlyMultiplier(), p2.getTooEarlyMultiplier());
+            }
+        });
+        betterLater.sort((p1, p2) -> {
+            if (p1.getTooLateMultiplier() == p2.getTooLateMultiplier()) {
+                return Integer.compare(p1.getDuration(), p2.getDuration());
+            } else {
+                return -Integer.compare(p1.getTooLateMultiplier(), p2.getTooLateMultiplier());
+            }
+        });
         LinkedList<Task> result = new LinkedList<>(betterEarlier);
         result.addAll(result.size(), betterLater);
         Instance resultInstance = new Instance(result.toArray(new Task[0])).expand();
-        canSwapNeighbourTask(resultInstance, boundary);
         return canSwapNeighbourTask(shiftStartPoint(resultInstance, boundary), boundary);
     }
 
-    private void sortOfRatio(Task[] tasks, int border){
+    private void sortOfRatio(Task[] tasks, int border) {
         for (Task task : tasks) {
             if (task.getRatio() < 1.0) {
-//                if (fitBeforeBorder(border, task)) {
-                    betterEarlier.addFirst(task);
-//                }else {
-//                    betterLater.addFirst(task);
-//                }
+                betterEarlier.addFirst(task);
             } else {
                 betterLater.addFirst(task);
             }
         }
     }
 
-    private Instance canSwapNeighbourTask(Instance instance, int boundary){
-        for (int i = 1;  i <  instance.getSizeInstance()-1; i++){
+    private Instance canSwapNeighbourTask(Instance instance, int boundary) {
+        for (int i = 1; i < instance.getSizeInstance() - 1; i++) {
             Instance tmp = instance.clone();
-            tmp.swapTasks(i-1, i);
-            if (instance.calcCost(boundary) > tmp.expand().calcCost(boundary)){
+            tmp.swapTasks(i - 1, i);
+            if (instance.calcCost(boundary) > tmp.expand().calcCost(boundary)) {
                 instance = tmp;
             }
         }
         return instance;
     }
 
-    private Instance shiftStartPoint(Instance instance, int boundary){
+    public static Instance shiftStartPoint(Instance instance, int boundary) {
         int bestStartPoint = 0;
         int currentResult = instance.calcCost(boundary);
-        instance.setStartPoint(bestStartPoint+1);
+        instance.setStartPoint(bestStartPoint + 1);
         instance.expand();
-        while (instance.calcCost(boundary) < currentResult){
+        while (instance.calcCost(boundary) < currentResult) {
             bestStartPoint = instance.getStartPoint();
             currentResult = instance.calcCost(boundary);
-            instance.setStartPoint(bestStartPoint+1);
+            instance.setStartPoint(bestStartPoint + 1);
             instance.expand();
         }
         instance.setStartPoint(bestStartPoint);
