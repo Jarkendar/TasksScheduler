a0c729f37397788e63f04a073340538b7eb3ea12
jaroslaw <jarek.skrzypczak@o2.pl>
2018-12-02 10:27:31 +0100

    Modify shift, now shift jump from point to point by step size 1,2,3,..., and try change direction

--- src/main/java/Shifter.java
+++ src/main/java/Shifter.java
@@ -1,14 +1,32 @@
 public class Shifter {
     public static Instance shiftStartPoint(Instance instance, int boundary) {
         int bestStartPoint = 0;
+        int shift = 1;
         int currentResult = instance.calcCost(boundary);
-        instance.setStartPoint(bestStartPoint + 1);
+        instance.setStartPoint(bestStartPoint + shift);
         instance.expand();
-        while (instance.calcCost(boundary) < currentResult) {
-            bestStartPoint = instance.getStartPoint();
-            currentResult = instance.calcCost(boundary);
-            instance.setStartPoint(bestStartPoint + 1);
-            instance.expand();
+        while (true) {
+            while (instance.calcCost(boundary) < currentResult) {
+                if (shift > 0){
+                    shift++;
+                }else {
+                    shift--;
+                }
+                bestStartPoint = instance.getStartPoint();
+                currentResult = instance.calcCost(boundary);
+                instance.setStartPoint(bestStartPoint + shift);
+                instance.expand();
+            }
+            if (Math.abs(shift) == 1){
+                break;
+            }
+            if (shift > 0){
+                shift = -1;
+                instance.setStartPoint(bestStartPoint + shift);
+            }else {
+                shift = 1;
+                instance.setStartPoint(bestStartPoint + shift);
+            }
         }
         instance.setStartPoint(bestStartPoint);
         return instance.expand();
