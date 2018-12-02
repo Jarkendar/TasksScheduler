ce30f2bb122d2e8fc69cf170d8a7a5ead7da28dc
jaroslaw <jarek.skrzypczak@o2.pl>
2018-12-02 12:37:14 +0100

    Add second sort to ratio scheduler

--- src/main/java/RatioScheduler.java
+++ src/main/java/RatioScheduler.java
@@ -14,38 +14,63 @@ public class RatioScheduler extends Scheduler {
     @Override
     public Instance scheduleTask(double h) {
         Task[] tasks = getOriginInstance().getTasks();
+        Task[] tasksWithout = getOriginInstance().clone().getTasks();
         int boundary = (int) (getOriginInstance().getDurationSum() * h);
-        sortOfRatio(tasks, boundary);
+
+        sortOfRatio(tasks);
         betterEarlier.sort((p1, p2) -> {
-            if (p1.getEarlyToDuration() == p2.getEarlyToDuration()){
+            if (p1.getEarlyToDuration() == p2.getEarlyToDuration()) {
                 if (p1.getTooEarlyMultiplier() == p2.getTooEarlyMultiplier()) {
                     return -Integer.compare(p1.getDuration(), p2.getDuration());
                 } else {
                     return Integer.compare(p1.getTooEarlyMultiplier(), p2.getTooEarlyMultiplier());
                 }
-            }else {
+            } else {
                 return Double.compare(p1.getEarlyToDuration(), p2.getEarlyToDuration());
             }
         });
         betterLater.sort((p1, p2) -> {
-            if (p1.getLateToDuration() == p2.getLateToDuration()){
+            if (p1.getLateToDuration() == p2.getLateToDuration()) {
                 if (p1.getTooLateMultiplier() == p2.getTooLateMultiplier()) {
                     return Integer.compare(p1.getDuration(), p2.getDuration());
                 } else {
                     return -Integer.compare(p1.getTooLateMultiplier(), p2.getTooLateMultiplier());
                 }
-            }else {
+            } else {
                 return Double.compare(-p1.getLateToDuration(), -p2.getLateToDuration());
             }
-
         });
-        LinkedList<Task> result = new LinkedList<>(betterEarlier);
-        result.addAll(result.size(), betterLater);
-        Instance resultInstance = new Instance(result.toArray(new Task[0])).expand();
-        return canSwapNeighbourTask(Shifter.shiftStartPoint(resultInstance, boundary), boundary);
+        LinkedList<Task> resultCostDuration = new LinkedList<>(betterEarlier);
+        resultCostDuration.addAll(resultCostDuration.size(), betterLater);
+        Instance instanceCostDuration = new Instance(resultCostDuration.toArray(new Task[0])).expand();
+        Instance swapCostDuration = canSwapNeighbourTask(Shifter.shiftStartPoint(instanceCostDuration, boundary), boundary);
+
+        betterEarlier.clear();
+        betterLater.clear();
+        sortOfRatio(tasksWithout);
+        betterEarlier.sort((p1, p2) -> {
+            if (p1.getTooEarlyMultiplier() == p2.getTooEarlyMultiplier()) {
+                return -Integer.compare(p1.getDuration(), p2.getDuration());
+            } else {
+                return Integer.compare(p1.getTooEarlyMultiplier(), p2.getTooEarlyMultiplier());
+            }
+        });
+        betterLater.sort((p1, p2) -> {
+            if (p1.getTooLateMultiplier() == p2.getTooLateMultiplier()) {
+                return Integer.compare(p1.getDuration(), p2.getDuration());
+            } else {
+                return -Integer.compare(p1.getTooLateMultiplier(), p2.getTooLateMultiplier());
+            }
+        });
+        LinkedList<Task> resultWithout = new LinkedList<>(betterEarlier);
+        resultWithout.addAll(resultWithout.size(), betterLater);
+        Instance instanceWithout = new Instance(resultWithout.toArray(new Task[0])).expand();
+        Instance swapWithout = canSwapNeighbourTask(Shifter.shiftStartPoint(instanceWithout, boundary), boundary);
+
+        return swapCostDuration.calcCost(boundary) < swapWithout.calcCost(boundary) ? swapCostDuration : swapWithout;
     }
 
-    private void sortOfRatio(Task[] tasks, int border) {
+    private void sortOfRatio(Task[] tasks) {
         for (Task task : tasks) {
             if (task.getRatio() < 1.0) {
                 betterEarlier.addFirst(task);
