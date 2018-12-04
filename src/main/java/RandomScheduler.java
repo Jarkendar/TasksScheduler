77a83d397a5b589d2b54a0ce08f2298e98600cf1
jaroslaw <jarek.skrzypczak@o2.pl>
2018-12-04 10:09:50 +0100

    Change manual randomize list to Collection shuffle

--- src/main/java/RandomScheduler.java
+++ src/main/java/RandomScheduler.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Random;
 
 public class RandomScheduler extends Scheduler {
 
@@ -12,13 +12,7 @@ public class RandomScheduler extends Scheduler {
     public Instance scheduleTask(double h) {
         Instance workInstance = getOriginInstance().clone();
         LinkedList<Task> workTask = new LinkedList<>(Arrays.asList(workInstance.getTasks()));
-        LinkedList<Task> tasks = new LinkedList<>();
-        Random random = new Random();
-        for (int i = 0; i < workInstance.getSizeInstance(); ++i){
-            int position = random.nextInt(workTask.size());
-            tasks.addFirst(workTask.get(position));
-            workTask.remove(position);
-        }
-        return new Instance(tasks.toArray(new Task[0])).expand();
+        Collections.shuffle(workTask);
+        return new Instance(workTask.toArray(new Task[0]));
     }
 }
