7bcb753d6c7e551afbe0c735a270cba8f8b022c6
jaroslaw <jarek.skrzypczak@o2.pl>
2018-12-05 17:39:25 +0100

    Fix bug with stop one point from better solution

--- src/main/java/Shifter.java
+++ src/main/java/Shifter.java
@@ -2,31 +2,36 @@ public class Shifter {
     public static Instance shiftStartPoint(Instance instance, int boundary) {
         int bestStartPoint = 0;
         int shift = 1;
-        int currentResult = instance.calcCost(boundary);
+        int currentBestResult = instance.calcCost(boundary);
+        byte changes = 0;
         instance.setStartPoint(bestStartPoint + shift);
         instance.expand();
         while (true) {
-            while (instance.calcCost(boundary) < currentResult) {
+            while (instance.calcCost(boundary) < currentBestResult) {
                 if (shift > 0){
                     shift++;
                 }else {
                     shift--;
                 }
                 bestStartPoint = instance.getStartPoint();
-                currentResult = instance.calcCost(boundary);
+                currentBestResult = instance.calcCost(boundary);
                 instance.setStartPoint(bestStartPoint + shift);
                 instance.expand();
+                changes=0;
             }
-            if (Math.abs(shift) == 1){
+            if (Math.abs(shift) == 1 && changes == 2){
                 break;
             }
             if (shift > 0){
                 shift = -1;
                 instance.setStartPoint(bestStartPoint + shift);
+                changes++;
             }else {
                 shift = 1;
                 instance.setStartPoint(bestStartPoint + shift);
+                changes++;
             }
+            instance.expand();
         }
         instance.setStartPoint(bestStartPoint);
         return instance.expand();
