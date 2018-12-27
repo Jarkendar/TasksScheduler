6f988bf7433cc7e93424d4cebd15725a548111cc
jaroslaw <jarek.skrzypczak@o2.pl>
2018-12-27 18:17:28 +0100

    Add run second algorithm in python as first, wait for result and read result from file Calc time first algorithm, rest time from 1 minute is for ACO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         if (args.length == 3) {
             double h = Double.parseDouble(args[2]);
-            Instance instance = new FileManager().readInstanceFromFile(args[0],Integer.parseInt(args[1]));
-            Instance resultRatio = new RatioScheduler(instance).scheduleTask(h);
+            int resultSignal = -1;
+            Instance instance;
+//            Instance resultRatio = new RatioScheduler(instance).scheduleTask(h);
+            long startTime = System.currentTimeMillis();
+            try {
+                Process process = Runtime.getRuntime().exec("python3 main.py "+args[0]+" "+args[1]+" "+args[2]+" javaResult");
+                resultSignal = process.waitFor();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            long firstPartTime = (System.currentTimeMillis() - startTime)/1000;
+            if (resultSignal == 0){
+                //read instance from file
+                instance = new FileManager().readInstanceFromFile("results/javaResult/"+args[0].split("\\.")[0]+"_"+args[1]+"_"+args[2].split("\\.")[1]+".txt");
+
+            }else {
+                instance = new FileManager().readInstanceFromFile(args[0],Integer.parseInt(args[1]));
+            }
 //            new FileManager().writeInstanceToFile(args[0].split("[.]")[0], Integer.parseInt(args[1]), resultRatio, h);
-            new FileManager().writeInstanceToFile(args[0].split("[.]")[0], Integer.parseInt(args[1]), new AntScheduler(instance, 58*1000, resultRatio).scheduleTask(h), h);
+            new FileManager().writeInstanceToFile(args[0].split("[.]")[0], Integer.parseInt(args[1]), new AntScheduler(instance, 60*1000-firstPartTime, instance).scheduleTask(h), h);
         }else if (args.length == 1){
             new FileManager().checkInstanceFromFile(args[0]);
         }else if (args.length == 0){
